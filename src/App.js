252a97b855a9995c16a4df0b0de8f31eeca0103e
RosemaryLy <rosey.ly@hotmail.com>
2021-08-04 11:21:09 -0400

    firebase config

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ import {useAuthState} from 'react-firebase-hooks/auth';
 import {useCollectionData} from 'react-firebase-hooks/firestore'
 
 firebase.initializeApp({
-  //config
+  apiKey: "AIzaSyC3qj_AyDdxwMzCmKk9NiujPP3tV7e6QM4",
+  authDomain: "fir-chat-app-acef6.firebaseapp.com",
+  projectId: "fir-chat-app-acef6",
+  storageBucket: "fir-chat-app-acef6.appspot.com",
+  messagingSenderId: "96799744896",
+  appId: "1:96799744896:web:6097152dee09ce6ad5d765",
+  measurementId: "G-8R9SLZJBS6"
 })
 
 const auth = firebase.auth();
