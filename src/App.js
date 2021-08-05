d709297a488d8e2a181e687c6fd56299248d7623
RosemaryLy <rosey.ly@hotmail.com>
2021-08-05 15:32:22 -0400

    changed colors and center datetime stamp

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cute Superchat💬</h1>
+        <h1>🌸💬 Let's Chat! 💬🌸</h1>
       </header>
       <SignOut />
       <section>
@@ -47,7 +47,8 @@ function SignIn() {
   return (
     <>
       <button className="sign-in"onClick={signInWithGoogle}>Sign in with Google</button>
-      <p>Do not violate the community guidelines or you will be banned for life!</p>
+      <div className="center">
+        Do not violate the community guidelines or you will be banned for life!</div>
     </>
   )
 
@@ -98,7 +99,7 @@ function ChatRoom() {
 
       <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
 
-      <button type="submit" disabled={!formValue|| formValue.replace(/^\s+/, '').replace(/\s+$/, '') === ''}>🕊️</button>
+      <button type="submit" disabled={!formValue|| formValue.replace(/^\s+/, '').replace(/\s+$/, '') === ''}>✉️</button>
 
     </form>
   </>)
@@ -107,12 +108,15 @@ function ChatRoom() {
 
 function ChatMessage(props) {
   const { text, uid, photoURL,createdAt } = props.message;
-  const timestamp = new Date(createdAt * 1000).toLocaleTimeString()
-
+  const datestamp = new Date().toDateString();
+  const timestamp = new Date(createdAt * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+  const datetime = datestamp+ " " + timestamp
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
   return (<>
-    <p className="timestamp">{timestamp}</p>
+  <div className="center">
+    <p className="timestamp">{datetime}</p>
+    </div>
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBob&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Heather&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Light'} alt="User" />
       <p>{text}
