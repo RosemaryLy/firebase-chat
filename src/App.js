b8d53a0e854dbd6264ff497ebde233e08171675f
RosemaryLy <rosey.ly@hotmail.com>
2021-08-19 19:05:10 -0400

    date test

--- src/App.js
+++ src/App.js
@@ -107,14 +107,13 @@ function ChatRoom() {
 function ChatMessage(props) {
   const { text, uid, photoURL,createdAt } = props.message;
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-  const datestamp = new Date(createdAt*1000).toDateString();
+  const datestamp = new Date(firebase.firestore.Timestamp.now()._seconds*1000).toLocaleDateString();
   const timestamp = new Date(createdAt * 1000).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
   const datetime = datestamp+ " " + timestamp
 
   return (<>
   <div className="center">
     <p className="timestamp">{datetime}</p>
-
     <div className={`message ${messageClass}`}>
       <img src={photoURL || 'https://avataaars.io/?avatarStyle=Circle&topType=LongHairBob&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=Heather&eyeType=Happy&eyebrowType=Default&mouthType=Default&skinColor=Light'} alt="User" />
       <p>{text}
